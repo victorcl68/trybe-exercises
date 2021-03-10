53f30c1c691193cf5ca219f3f2a92afaa6f143a9
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 16:47:42 -0300

    Criação de programa que retorne o maior de três números

--- Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
@@ -1,3 +1,4 @@
+//
 let a = 10;
 let b = 5;
 
@@ -7,7 +8,6 @@ console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
 
-//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 let c = 15;
 let d = 20;
 
@@ -16,4 +16,18 @@ if (c > d) {
 }
 else if (d > c) {
   console.log(d)
+}
+
+let e = 70;
+let f = 60;
+let g = 50;
+
+if (e > f && e > g) {
+  console.log(e);
+}
+else if (f > e && f > g) {
+  console.log(f);
+}
+else {
+  console.log(g);
 }
