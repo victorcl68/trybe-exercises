541e055084ef954cffda28e5ae094571d61e8a0c
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 19:25:12 -0300

    Quarto exercício maior ou menos que 20

--- Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
@@ -15,3 +15,17 @@ let somaparamedia = 0;
 for (index = 0; index < numbers.length; index +=1)
   somaparamedia = somaparamedia + numbers[index];
 console.log(somaparamedia/numbers.length);
+
+//Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+let somaparamedia = 0;
+let media = 0;
+for (index = 0; index < numbers.length; index +=1)
+  somaparamedia = somaparamedia + numbers[index];
+media = somaparamedia/numbers.length;
+console.log(media);
+if (media > 20) {
+  console.log('valor maior que 20');
+}
+else if (media < 20) {
+  console.log('valor menor que 20');
+}
