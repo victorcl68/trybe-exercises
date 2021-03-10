3729f2adbba26123c1db3f69ca6c4b2bc5eb78f0
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 16:53:18 -0300

    Criação de programa que retorna se número é positivo, negativo ou zero

--- Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
@@ -1,4 +1,4 @@
-//
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados.
 let a = 10;
 let b = 5;
 
@@ -8,6 +8,7 @@ console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
 
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 let c = 15;
 let d = 20;
 
@@ -18,6 +19,7 @@ else if (d > c) {
   console.log(d)
 }
 
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
 let e = 70;
 let f = 60;
 let g = 50;
@@ -30,4 +32,17 @@ else if (f > e && f > g) {
 }
 else {
   console.log(g);
+}
+
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+let number = 0;
+
+if (number > 0) {
+  console.log('positive');
+}
+else if (number < 0) {
+  console.log('negative');
+}
+else {
+  console.log('zero');
 }
