88950ae3dd02d14adde9334d993292ae66f592d8
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 16:39:26 -0300

    Criação de programa que retorne o maior de dois números

---
+++ Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
+
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+let c = 15;
+let d = 20;
+
+if (c > d) {
+  console.log(c)
+}
+else if (d > c) {
+  console.log(d)
+}
