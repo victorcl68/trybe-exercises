be7b513959d4bf91225114ce5f22b8f4f278da2d
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 17:40:43 -0300

    Criação de programa que converte nota em porcentagem em conceitos de A até F

--- Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.1/script.js
@@ -87,3 +87,27 @@ switch (peça) {
   default:
     console.log('error');
 }
+
+//Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.
+let nota = 101;
+if (nota >= 90 && nota <= 100) {
+  console.log('A');
+}
+else if (nota >= 80 && nota <= 100) {
+  console.log('B');
+}
+else if (nota >= 70 && nota <= 100) {
+  console.log('C');
+}
+else if (nota >= 60 && nota <= 100) {
+  console.log('D');
+}
+else if (nota >= 50 && nota <= 100) {
+  console.log('E');
+}
+else if (nota < 50 && nota >=0) {
+  console.log('F');
+}
+else {
+  console.log('Erro');
+}
