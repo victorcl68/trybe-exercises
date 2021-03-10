71cbff933409285ce357186393a06f14572a3c5b
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-10 19:18:57 -0300

    Terceiro exercício média números

--- Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
@@ -1,9 +1,17 @@
+//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (index = 0; index < numbers.length; index +=1)
   console.log(numbers[index]);
 
+//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 let soma = 0;
 for (index = 0; index < numbers.length; index +=1)
   soma = soma + numbers[index];
 console.log(soma);
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let somaparamedia = 0;
+for (index = 0; index < numbers.length; index +=1)
+  somaparamedia = somaparamedia + numbers[index];
+console.log(somaparamedia/numbers.length);
