ca040e45e8189ee7eabbd472d9e62b3bc76467d5
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-11 01:34:01 -0300

    Exercício bônus 2 ordenar em ordem decrescente

--- Fund_Desenv_Web/exercises/Bloco_04/4.2/bonusscript.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.2/bonusscript.js
@@ -11,3 +11,15 @@ for (let i = 1; i < numbers.length; i++) {
   }
 }
 console.log(numbers);
+
+for (let i = 1; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] > numbers[j]) {
+      let position = numbers[i];
+
+      numbers[i] = numbers[j];
+      numbers[j] = position;
+    }
+  }
+}
+console.log(numbers);
