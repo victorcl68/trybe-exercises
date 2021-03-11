75f3c541ada979758e5830c9b6878767bcd94d9c
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-11 03:33:06 -0300

    Exercício 4 resolução

--- Fund_Desenv_Web/exercises/Bloco_04/4.3/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.3/script.js
@@ -32,3 +32,25 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   inputLine = '';
   inputPosition -= 1;
 };
+
+
+let n = 5;
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + '*';
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
