5ae7dd9275dee8871739cb761cc8f78cbd71e42f
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-11 03:09:31 -0300

    Exercício 3 resolução

--- Fund_Desenv_Web/exercises/Bloco_04/4.3/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.3/script.js
@@ -15,3 +15,20 @@ for (let index = 0; index <= n; index += 1) {
   console.log(inputLine);
   inputLine = inputLine + '*';
 };
+
+
+let n = 5;
+let inputLine = '';
+let inputPosition = n;
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine += ' ';
+    } else {
+      inputLine += '*';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
