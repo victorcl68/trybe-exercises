6d15a4ef136d07b57cd9dcab750829d2e38bec08
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-11 00:41:21 -0300

    Sexto exercício valores ímpares no array

--- Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
+++ Fund_Desenv_Web/exercises/Bloco_04/4.2/script.js
@@ -37,3 +37,16 @@ for (index = 1; index < numbers.length; index +=1)
     bigger = numbers[index];
   }
 console.log(bigger);
+
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let impar = 0
+for (index = 0; index < numbers.length; index +=1)
+  if (numbers[index] % 2 !== 0) {
+    impar += 1;
+  }
+if (impar === 0){
+  console.log('nenhum valor ímpar encontrado')
+}
+else{
+  console.log(impar)
+}
