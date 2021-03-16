1f0865874a4b0a851159b3979c4703bb2d02df82
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-16 18:41:44 -0300

    Resolução exercício 1 Bônus Calendário Tryber

--- Fund_Desenv_Web/exercises/Bloco_05/5.3/script.js
+++ Fund_Desenv_Web/exercises/Bloco_05/5.3/script.js
@@ -167,3 +167,33 @@ function setDayColor() {
     }
   };
 };
+
+function addNewTask() {
+  let getInputField = document.querySelector('#task-input');
+  let addInputButton = document.querySelector('#btn-add');
+  let getTaskList = document.querySelector('.task-list');
+
+  addInputButton.addEventListener('click', addTextTasks);
+  function addTextTasks (){
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    };
+  };
+
+  getInputField.addEventListener('keyup', enterKey); 
+  function enterKey (event) {
+    if (event.keyCode === 13 && getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    };
+  };
+};
+
+addNewTask();
