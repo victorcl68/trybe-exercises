1b9fba7a5a4424ba580e44d43770c46813461723
Victor Lopes <victor.cabrera0209@gmail.com>
2021-04-28 02:26:32 -0300

    PropType addition

--- Desenvolvimento_Front-end/exercises/Bloco_11/11.2/my-pokedex/src/components/Pokemon.js
+++ Desenvolvimento_Front-end/exercises/Bloco_11/11.2/my-pokedex/src/components/Pokemon.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+
 class PokemonClass extends Component {
   render() {
     const { PokemonInformation: { name, type, image, moreInfo, averageWeight: { value, measurementUnit } }} = this.props;
@@ -13,6 +15,19 @@ class PokemonClass extends Component {
       </div>
     );
   }
+}
+
+PokemonClass.propTypes = {
+  PokemonInformation: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    }),
+    image: PropTypes.string,
+    moreInfo: PropTypes.string,
+  }).isRequired,
 }
 
 export default PokemonClass;
